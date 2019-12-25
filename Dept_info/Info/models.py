dfe441a846fafcd3868e0384a491c51ed7631744
Nazmul <nazmulmoon123@gmail.com>
2019-12-25 21:14:15 +0600

    new

--- Dept_info/Info/models.py
+++ Dept_info/Info/models.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
 
 class Student(models.Model):
-    Id = models.CharField(max_length=100)
+    reg_roll = models.CharField(max_length=100)
     description = models.TextField()
 
+
     def __str__(self):
-        return self.title
+        return self.reg_roll
