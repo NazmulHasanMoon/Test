d6a106e1000eab0ecca1160ae30fb9873f8c4317
Sohel Suvro <suvro047@gmail.com>
2019-12-27 15:34:19 +0600

    new

--- Cse_dept/CSE/Info/views.py
+++ Cse_dept/CSE/Info/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Student
 
+
 def home(request):
     return render(request, 'home.html')
 
 
 def student_list(request):
     students = Student.objects.all()
-    return render(request,'student_list.html', {'all_student_list': students})
+    return render(request, 'student_list.html', {'all_student_list': students})
 
 
 def single_student(request, student_id):
-    post = Student.objects.get(pk=student_id)
-    return render(request, 'single_student.html', {'student': post})
+    student = Student.objects.get(pk=student_id)
+    return render(request, 'single_student.html', {'student': student})
